c089ba48486f35a6f4aa45d5a1c007cd05ccb5a5
Neodynamic Dev Team <neodynamic@users.noreply.github.com>
2020-07-10 12:25:57 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,8 +35,8 @@ export class AppComponent implements OnInit {
     } else if (JSPM.JSPrintManager.websocket_status === JSPM.WSStatus.Closed) {
         alert('JSPrintManager (JSPM) is not installed or not running! Download JSPM Client App from https://neodynamic.com/downloads/jspm');
         return false;
-    } else if (JSPM.JSPrintManager.websocket_status === JSPM.WSStatus.BlackListed) {
-        alert('JSPM has blacklisted this website!');
+    } else if (JSPM.JSPrintManager.websocket_status === JSPM.WSStatus.Blocked) {
+        alert('JSPM has blocked this website!');
         return false;
     }
   }
